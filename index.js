b2b89f44d7a845bc0cbc75219a517b358e3e0255
Juma-islam <ononnaislamj@gmail.com>
2025-11-10 20:43:26 +0600

    added update API

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ async function run() {
     const db = client.db("clean-connect");
     const issuesCollection = db.collection("issues");
     const contributionCollection = db.collection("contribution");
-   
 
     // all issues
     app.get("/issues", async (req, res) => {
@@ -35,13 +34,13 @@ async function run() {
 
     // issue details
     app.get("/issues/:id", async (req, res) => {
-      const {id} = req.params;
-      console.log(id)
-      const objectId = new ObjectId(id)
-      const result = await issuesCollection.findOne({_id : objectId});
+      const { id } = req.params;
+      console.log(id);
+      const objectId = new ObjectId(id);
+      const result = await issuesCollection.findOne({ _id: objectId });
       res.send({
         success: true,
-        result
+        result,
       });
     });
 
@@ -56,20 +55,57 @@ async function run() {
       });
     });
 
-// modal contribution 
-app.post("/contributions", async(req, res) => {
-  const data = req.body;
-  const result = await contributionCollection.insertOne(data);
-  res.send({success: true, result})
-});
+    // modal contribution
+    app.post("/contributions", async (req, res) => {
+      const data = req.body;
+      const result = await contributionCollection.insertOne(data);
+      res.send({ success: true, result });
+    });
 
-// modal contribution id
-app.get("/contributions/:issueId", async(req, res) =>{
-  const {issueId} = req.params;
-  const result = await contributionCollection.find({issueId: issueId}).toArray();
-  res.send({success: true, result})
-});
+    // modal contribution id
+    app.get("/contributions/:issueId", async (req, res) => {
+      const { issueId } = req.params;
+      const result = await contributionCollection.find({ issueId: issueId }).toArray();
+      res.send({ success: true, result });
+    });
 
+    // My issue
+    app.get("/my-issues/:email", async (req, res) => {
+      const { email } = req.params;
+      const result = await issuesCollection.find({ email }).toArray();
+      res.send({ success: true, result });
+    });
+
+    // Update issue
+    // app.put("/issues/:id", async (req, res) => {
+    //   const { id } = req.params;
+    //   const updateData = req.body;
+    //   const result = await issuesCollection.updateOne(
+    //     { _id: new ObjectId(id) },
+    //     { $set: updateData }
+    //   );
+    //   res.send({ success: true, result });
+    // });
+    app.put("/issues/:id", async (req, res) => {
+      const { id } = req.params;
+      const updateData = req.body;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: updateData,
+      };
+      const result = await modelCollection.updateOne(filter, update);
+      res.send({ success: true, result });
+    });
+
+    // Delete issue by id
+    // app.delete("/issues/:id", async (req, res) => {
+    //   const { id } = req.params;
+    //   const result = await issuesCollection.deleteOne({ _id: new ObjectId(id) });
+    //   res.send({ success: true, result });
+    // });
+
+    // akan theke chaatgpt sesh
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
